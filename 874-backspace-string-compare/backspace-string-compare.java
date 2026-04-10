cca60e67d4ba4aa14b53bae9b9a7bac6d4fb765c
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-04-10 13:55:51 +0530

    Time: 2 ms (57.67%) | Memory: 43.4 MB (21.96%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,36 +1,29 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i = s.length()-1;
-        int j = t.length()-1;
-        int skipS = 0, skipT = 0;
+        // time: O(n), n = number of characters to loop through
+        // space:O(n), n = number of characters store in a Stack
 
-        while(i >= 0 || j >= 0){
+        // s = ab#c, t = ad#c
+        // s = ac    t = ac
 
-            while(i >= 0){
-                if(s.charAt(i) == '#'){
-                    skipS++; i--;
-                }else if(skipS > 0){
-                    skipS--; i--;
-                }else break;
+        // equals method checks content 
+        return buildStack(s).equals(buildStack(t));
+    }
+
+    public Stack<Character> buildStack(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : str.toCharArray()) {
+            if(c == '#' && !stack.isEmpty()) {
+                stack.pop();
+                continue;  
             }
 
-            while(j >= 0){
-                if(t.charAt(j) == '#'){
-                    skipT++; j--;
-                }else if(skipT > 0){
-                    skipT--; j--;
-                }else break;
+            if(c != '#') {
+            stack.push(c);
             }
-
-            if(i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j))
-                return false;
-
-            if((i >= 0) != (j >= 0))
-                return false;
-
-            i--; 
-            j--;
         }
-        return true;
+
+        return stack;
     }
 }
