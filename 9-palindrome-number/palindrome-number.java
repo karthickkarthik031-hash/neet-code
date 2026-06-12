1e721d268dae025708fd4668cb79d7289adbd195
karthickkarthik031-hash <karthickkarthik031@gmail.com>
2026-06-12 12:25:40 +0530

    Time: 5 ms (78.89%) | Memory: 46.1 MB (17.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         int a=0;
         int b=x;
         int sum=0;
         while(x>0){
-            a=x%10;
-            sum=sum*10+a;
+            sum=sum*10+x%10;
             x=x/10;
         }
         if(sum==b){
